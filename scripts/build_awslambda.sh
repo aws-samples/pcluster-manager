1b7bb45772bb078bbf33c6b43a556ac41536e92e
Charlie Gruenwald <cgruenwa@amazon.com>
2021-11-16 16:51:24 -0700

    [scripts/build_awslambda] Remove unused code.

--- scripts/build_awslambda.sh
+++ scripts/build_awslambda.sh
@@ -13,17 +13,6 @@ docker build --build-arg PUBLIC_URL=. -t frontend-awslambda .
 popd
 docker build -f Dockerfile.awslambda -t ${ECR_REPO} .
 
-IMAGE=${ECR_ENDPOINT}/pcluster-manager-e38c2090-31f6-11ec-aa59-0626fd810c34:latest
-LAMBDA_ARN=arn:aws:lambda:us-east-2:538173551358:function:PclusterManagerFunction-e38c2090-31f6-11ec-aa59-0626fd810c34
-
-# The following lines use the two settings above to re-upload the image to docker
-# aws ecr get-login-password | docker login --username AWS --password-stdin "${ECR_ENDPOINT}"
-# docker tag ${ECR_REPO} ${IMAGE}
-# docker push ${IMAGE}
-# aws lambda update-function-code --function-name ${LAMBDA_ARN}  --image-uri ${IMAGE} --publish
-# 
-# exit 0
-
 # These upload the container to the public repo
 ECR_ENDPOINT="public.ecr.aws/n0x0o5k1"
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin "${ECR_ENDPOINT}"
