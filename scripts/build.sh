616d06edce1717977beb351f5ada5107b1d02994
Charlie Gruenwald <cgruenwa@amazon.com>
2022-02-11 19:31:34 -0700

    [ecr-pull-through] checkpoint docker commands.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@
 ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 ECR_ENDPOINT="${ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
 ECR_REPO=pcluster-manager
+ECR_BASE=public.ecr.aws/n0x0o5k1
 
 pushd frontend
 docker build -t frontend .
@@ -13,5 +14,5 @@ docker build -t ${ECR_REPO} .
 #docker push ${ECR_ENDPOINT}/${ECR_REPO}:latest
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/n0x0o5k1
-docker tag pcluster-manager:latest public.ecr.aws/n0x0o5k1/pcluster-manager:latest
-docker push public.ecr.aws/n0x0o5k1/pcluster-manager:latest
+docker tag $(ECR_REPO):latest $(ECR_BASE)/$(ECR_REPO):latest
+docker push $(ECR_BASE)/$(ECR_REPO):latest
