02c66e3efe0165754e56b309b28ab10938c9a294
Charlie Gruenwald <cgruenwa@amazon.com>
2022-02-20 04:38:19 -0700

    `[infrastructure]` Factor the upload script.

--- infrastructure/upload.sh
+++ infrastructure/upload.sh
@@ -5,6 +5,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 REGIONS=( $(aws ec2 describe-regions --query "Regions[*].RegionName" --output text) )
 #REGIONS=(us-east-1)
 
+FILES=(SSMSessionProfile-cfn.yaml pcluster-manager-cognito.yaml pcluster-manager.yaml)
+
 for REGION in "${REGIONS[@]}"
 do
     if [ "$REGION" != "ap-southeast-3" ];
@@ -12,8 +14,9 @@ do
         AWS_DEFAULT_REGION=${REGION}
         BUCKET=pcluster-manager-${REGION}
         echo Uploading to: ${BUCKET}
-        aws s3 cp --acl public-read ${SCRIPT_DIR}/SSMSessionProfile-cfn.yaml s3://${BUCKET}/SSMSessionProfile-cfn.yaml
-        aws s3 cp --acl public-read ${SCRIPT_DIR}/pcluster-manager-cognito.yaml s3://${BUCKET}/pcluster-manager-cognito.yaml
-        aws s3 cp --acl public-read ${SCRIPT_DIR}/pcluster-manager.yaml s3://${BUCKET}/pcluster-manager.yaml
+        for FILE in "${FILES[@]}"
+        do
+          aws s3 cp --acl public-read ${SCRIPT_DIR}/${FILE} s3://${BUCKET}/${FILE}
+        done
     fi
 done
