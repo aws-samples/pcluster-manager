ffbae9e8bce6141788cce8660920df46c3ba46de
Charlie Gruenwald <cgruenwa@amazon.com>
2022-02-23 13:48:18 -0700

    [spack] remove debug print.

--- resources/scripts/spack.sh
+++ resources/scripts/spack.sh
@@ -4,14 +4,9 @@
 export USER=$cfn_cluster_user
 export SPACK_ROOT=${1}
 
-echo spack_root: -${SPACK_ROOT}-
-echo user: $USER
-
 mkdir -p $SPACK_ROOT
 git clone -c feature.manyFiles=true https://github.com/spack/spack $SPACK_ROOT
 cd $SPACK_ROOT
-
-ls /home
 
 echo "export SPACK_ROOT=$SPACK_ROOT" >> /home/$USER/.bashrc
 echo "source \$SPACK_ROOT/share/spack/setup-env.sh" >> /home/$USER/.bashrc
