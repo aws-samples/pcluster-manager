48b7cb5b6457559b7f54d833b06589309b2e3ba5
Charlie Gruenwald <cgruenwa@amazon.com>
2022-03-10 07:28:15 -0700

    `[post-install/sacct]` Use Chef for cross-platform support.

---
+++ resources/files/sacct/slurm_accounting.rb
@@ -0,0 +1,54 @@
+require 'json'
+return if node['cluster']['node_type'] != 'HeadNode'
+
+# Get Slurm database credentials
+secret = JSON.parse(shell_out!("aws secretsmanager get-secret-value --secret-id #{node['secret_id']} --region #{node['cluster']['region']} --query SecretString --output text").stdout)
+
+case node['platform']
+when 'ubuntu'
+  package 'mysql-client'
+when 'amazon', 'centos'
+  package 'mysql'
+end
+
+template '/tmp/slurm_sacct.conf' do
+  source '/tmp/slurm_sacct.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0600'
+  variables(
+    slurm_db_user: secret['username'],
+    slurm_dbd_host: shell_out!('hostname').stdout.strip
+  )
+  local true
+end
+
+template '/tmp/slurmdbd.conf' do
+  source '/tmp/slurmdbd.conf.erb'
+  owner 'slurm'
+  group 'slurm'
+  mode '0600'
+  variables(
+    slurm_db_user: secret['username'],
+    slurm_db_password: secret['password'],
+    slurm_db_host: node['ec2']['local_hostname'],
+    slurm_dbd_host: shell_out!('hostname').stdout.strip
+  )
+  sensitive true
+  local true
+end
+
+file '/etc/systemd/system/slurmdbd.service' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  content ::File.open('/tmp/slurmdbd.service').read
+end
+
+service 'slurmdbd' do
+  action :start
+end
+
+service 'slurmctld' do
+  action :restart
+end
--- resources/scripts/slurm-accounting.sh
+++ resources/scripts/slurm-accounting.sh
@@ -3,40 +3,38 @@
 set -x
 set -e
 
-# Install MySQL
-yum install -y mysql
-
 # Set variables from post-install args
 secret_id=$1
 rds_endpoint=$2
 rds_port=$3
 
-# Get Slurm database credentials
-slurm_db_user=$(aws secretsmanager get-secret-value --secret-id ${secret_id} --region ${cfn_region} | jq --raw-output '.SecretString' | jq -r .username)
-slurm_db_password=$(aws secretsmanager get-secret-value --secret-id ${secret_id} --region ${cfn_region} | jq --raw-output '.SecretString' | jq -r .password)
+mkdir -p /tmp/slurm_accounting
+pushd /tmp/slurm_accounting
 
-# Other variables needed for configuring Slurm
-slurm_dbd_host=$(hostname)
-slurm_etc=/opt/slurm/etc
+cat <<EOF > /tmp/sacct_attrs.json
+{
+  "cluster": {"accounting": {
+  "secret_id": "${secret_id}",
+  "rds_endpoint": "${rds_endpoint}",
+  "rds_port": "${rds_port}"}}
+}
+EOF
 
 # Copy Slurm configuration files
 source_path=https://raw.githubusercontent.com/aws-samples/pcluster-manager/post-install-scripts/resources/files
-wget -qO- ${source_path}/sacct/slurm_sacct.conf > ${slurm_etc}/slurm_sacct.conf
-wget -qO- ${source_path}/sacct/slurmdbd.conf > ${slurm_etc}/slurmdbd.conf
+files=(slurm_sacct.conf.erb slurmdbd.service slurmdbd.conf.erb  slurm_accounting.rb)
+for file in "${files[@]}"
+do
+    wget -qO- ${source_path}/sacct/${file} > ${file}
+done
 
-# Modify Slurm configuration files
-sed -i "s|@SLURM_DBD_HOST@|${slurm_dbd_host}|g" ${slurm_etc}/slurm_sacct.conf
-sed -i "s|@SLURM_DBD_USER@|${slurm_db_user}|g" ${slurm_etc}/slurm_sacct.conf
-sed -i "s|@SLURM_DBD_HOST@|${slurm_dbd_host}|g" ${slurm_etc}/slurmdbd.conf
-sed -i "s|@RDS_USER@|${slurm_db_user}|g" ${slurm_etc}/slurmdbd.conf
-sed -i "s|@RDS_PASS@|${slurm_db_password//|/\\|}|g" ${slurm_etc}/slurmdbd.conf
-sed -i "s|@RDS_ENDPOINT@|${rds_endpoint}|g" ${slurm_etc}/slurmdbd.conf
-sed -i "s|@RDS_PORT@|${rds_port}|g" ${slurm_etc}/slurmdbd.conf
-
-echo "include slurm_sacct.conf" >> ${slurm_etc}/slurm.conf
-chmod 600 ${slurm_etc}/slurmdbd.conf
-chown slurm:slurm ${slurm_etc}/slurmdbd.conf
-
-# Restart Slurm daemons
-/opt/slurm/sbin/slurmdbd
-systemctl restart slurmctld
+sudo cinc-client \
+  --local-mode \
+  --config /etc/chef/client.rb \
+  --log_level auto \
+  --force-formatter \
+  --no-color \
+  --chef-zero-port 8889 \
+  --json-attributes /etc/chef/dna.json \
+  -j /tmp/slurm_accounting/sacct_attrs.json \
+  -z slurm_accounting.rb
