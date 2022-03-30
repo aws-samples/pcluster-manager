496d7dc139ca106f59dabcaf643a91648a495727
Charlie Gruenwald <cgruenwa@amazon.com>
2022-03-29 19:44:45 -0600

    `[Logs]` Better handle no streams.

--- frontend/src/pages/Clusters/Logs.js
+++ frontend/src/pages/Clusters/Logs.js
@@ -152,7 +152,7 @@ function LogEvents() {
 function StreamList({instanceId}) {
   const logStreamIndex = useState(['app', 'clusters', 'logs', 'index']);
   const logStreams = useState(['app', 'clusters', 'logs', 'index', instanceId, 'streams']) || [];
-  const ip = logStreamIndex[instanceId].ip;
+  const ip = useState(['app', 'clusters', 'logs', 'index', instanceId, 'ip']);
   const fnames = Object.keys(logStreams).sort()
   const selectedLogStreamName = useState(['app', 'clusters', 'selectedLogStreamName']);
   let [searchParams, setSearchParams] = useSearchParams();
