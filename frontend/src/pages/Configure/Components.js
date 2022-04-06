f4e76897ae3add65bcb814dd9744e19c770d293b
Charlie Gruenwald <cgruenwa@amazon.com>
2022-04-06 13:43:51 -0600

    [slurm-accounting] Update documentation to reflect changes to CFN stack.

--- frontend/src/pages/Configure/Components.js
+++ frontend/src/pages/Configure/Components.js
@@ -20,6 +20,7 @@ import { setState, getState, useState, updateState, clearState, clearEmptyNest }
 
 // UI Elements
 import {
+  Alert,
   Autosuggest,
   Button,
   FormField,
@@ -397,7 +398,7 @@ function ActionEditor({label, actionKey, errorPath, path}) {
 
   return <>
       <FormField label={<div>{label} <Toggle checked={useMultiRunner} onChange={toggleUseMultiRunner}>Use Multi-Script Runner?</Toggle></div>} errorText={errorPath}>
-        {useMultiRunner && <div><b>Experimental!</b> The Multi-Script Runner is experimental and uses scripts stored as a sibling <Link external href={script}>here</Link> which are maintained separate from the AWS ParallelCluster project. Please evaluate these scripts before running them and valiate that they are compatible with your environment.</div>}
+        {useMultiRunner && <div style={{marginBottom: "10px"}}><Alert><b>Experimental!</b> The Multi-Script Runner is experimental and uses scripts stored as a sibling <Link external href={script}>here</Link> which are maintained separate from the AWS ParallelCluster project. Please evaluate these scripts before running them and valiate that they are compatible with your environment.</Alert></div>}
         {useMultiRunner && <MultiRunnerEditor path={[...path, 'Args']}/>}
         {!useMultiRunner &&
         <SpaceBetween direction="vertical" size="xs">
