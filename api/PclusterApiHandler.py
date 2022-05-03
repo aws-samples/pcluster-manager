d51d9907beb7ee78d1417254c6416cc03ccab3f1
Sean Smith <seaam@amazon.com>
2022-05-03 09:44:59 -0700

    Add t3 instances
    Signed-off-by: Sean Smith <seaam@amazon.com>

--- api/PclusterApiHandler.py
+++ api/PclusterApiHandler.py
@@ -493,7 +493,7 @@ def get_instance_types():
         ec2 = boto3.client("ec2")
     filters = [
         {"Name": "current-generation", "Values": ["true"]},
-        {"Name": "instance-type", "Values": ["c5*", "c6*", "g4*", "g5*", "hpc*", "p3*", "p4*", "t2*", "m6*", "r*"]},
+        {"Name": "instance-type", "Values": ["c5*", "c6*", "g4*", "g5*", "hpc*", "p3*", "p4*", "t2*", "t3*", "m6*", "r*"]},
     ]
     instance_paginator = ec2.get_paginator("describe_instance_types")
     instances_paginator = instance_paginator.paginate(Filters=filters)
