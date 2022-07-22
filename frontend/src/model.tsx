1a76d51b5e9827c596afc0b5b67facde9ee910e6
Tommaso Scarlatti <tsscarla@amazon.com>
2022-07-22 16:05:00 +0200

    refactor: leverage useQuery in ListClusters()

--- frontend/src/model.tsx
+++ frontend/src/model.tsx
@@ -163,19 +163,18 @@ function DeleteCluster(clusterName: any, callback=null) {
   })
 }
 
-function ListClusters(callback: any) {
+async function ListClusters() {
   var url = 'api?path=/v3/clusters';
-  request('get', url).then((response: any) => {
-    //console.log("List Success", response)
-    if(response.status === 200) {
-      callback && callback(response.data.clusters);
-      setState(['clusters', 'list'], response.data.clusters);
+  try {
+    const { data } = await request('get', url);
+    setState(['clusters', 'list'], data?.clusters);
+    return data?.clusters || [];
+  } catch (error) {
+    if((error as any).response) {
+      notify(`Error: ${(error as any).response.data.message}`, 'error');
     }
-  }).catch((error: any) => {
-    if(error.response)
-      notify(`Error: ${error.response.data.message}`, 'error');
-    console.log(error)
-  });
+    throw error;
+  }
 }
 
 function GetConfiguration(clusterName: any, callback=null) {
