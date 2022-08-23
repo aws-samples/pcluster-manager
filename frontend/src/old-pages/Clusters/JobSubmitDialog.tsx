d10c21bad9c27cd7f4af7f851ef0e36291be5f32
rkilpadi <rkilpadi@amazon.com>
2022-08-23 12:18:04 -0700

    Add UI option for script entry through Slurm API
    Also added i18next translation to JobSubmitDialog.tsx

--- frontend/src/old-pages/Clusters/JobSubmitDialog.tsx
+++ frontend/src/old-pages/Clusters/JobSubmitDialog.tsx
@@ -10,9 +10,13 @@
 // limitations under the License.
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useState, setState, getState, clearState } from '../../store'
-import { findFirst, clusterDefaultUser } from '../../util'
-import { SubmitJob, PriceEstimate } from '../../model'
+import { useState, setState, getState, clearState } from '../../store';
+import { findFirst, clusterDefaultUser } from '../../util';
+import { getScripts } from './util';
+import { SubmitJob, SubmitJobScript, PriceEstimate } from '../../model';
+import ConfigView from '../../components/ConfigView';
+import { Job } from '../../types/jobs';
+import FileUploadButton from '../../components/FileChooser'; 
 
 // UI Elements
 import {
@@ -38,19 +42,18 @@ function itemToOption([value, title]: [string, string]) {
 }
 
 function QueueSelect() {
+  const { t } = useTranslation();
   const clusterName = getState(['app', 'clusters', 'selected']);
   const clusterPath = ['clusters', 'index', clusterName];
   const queues = getState([...clusterPath, 'config', 'Scheduling', 'SlurmQueues']) || []
 
-  const jobPath = [...submitPath, 'job'];
   let partition = useState([...jobPath, 'partition']);
 
   let queuesOptions = [["[ANY]", "[ANY]"], ...queues.map((q: any) => [q.Name, q.Name])]
 
   return <>
-    {/* @ts-expect-error TS(2322) FIXME: Type '"h4"' is not assignable to type 'Variant | u... Remove this comment to see the full error message */}
-    <Header variant="h4"
-      description="Queue where the job will run.">Queue</Header>
+    <Header variant="h2"
+      description={t("JobSubmitDialog.queue.description")}>{t("JobSubmitDialog.queue.header")}</Header>
     <Select
       selectedOption={itemToOption(findFirst(queuesOptions, (x: any) => {return x[0] === partition}) || ["[ANY]", "[ANY]"])}
       onChange={({detail}) => {
@@ -67,6 +70,7 @@ function QueueSelect() {
 }
 
 function JobCostEstimate() {
+  const { t } = useTranslation();
   const jobRuntimePath = [...submitPath, 'jobRuntime'];
   const priceEstimatePath = [...submitPath, 'priceEstimate'];
   const priceEstimate = useState(priceEstimatePath);
@@ -75,7 +79,6 @@ function JobCostEstimate() {
   const costEstimatePending = useState(costEstimatePendingPath);
 
   const jobRuntime = useState(jobRuntimePath);
-  const jobPath = [...submitPath, 'job'];
   const nodes = useState([...jobPath, 'nodes']);
 
   const costEstimatePath = [...submitPath, 'costEstimate'];
@@ -100,15 +103,15 @@ function JobCostEstimate() {
     }
     if(!queueName || queueName === "[ANY]")
     {
-      setState(errorsPath, "Error: You must select a queue.");
+      setState(errorsPath, t("JobSubmitDialog.errors.mustSelectQueue"));
       clearState(costEstimatePendingPath);
     } else if(!nodes || nodes === '')
     {
-      setState(errorsPath, "Error: You must select a node count.");
+      setState(errorsPath, t("JobSubmitDialog.errors.mustSelectNodes"));
       clearState(costEstimatePendingPath);
     } else if(!jobRuntime || jobRuntime === '')
     {
-      setState(errorsPath, "Error: You must select a job runtime.");
+      setState(errorsPath, t("JobSubmitDialog.errors.mustSelectRuntime"));
       clearState(costEstimatePendingPath);
     } else {
       PriceEstimate(clusterName, queueName, callback, failure);
@@ -116,12 +119,12 @@ function JobCostEstimate() {
   }
 
   return <>
-    <Alert visible header="Experimental!">
-      This provides a basic cost estimate based on the expected job run-time, the number of nodes and their instance type. Actual costs will vary based on node uptime, storage, and other factors. Please refer to Cost Explorer for actual cluster costs.
+    <Alert visible header={t("JobSubmitDialog.costEstimate.alertHeader")}>
+      {t("JobSubmitDialog.costEstimate.alertContent")}
     </Alert>
     <div style={{marginTop: "10px"}}>
       <FormField errorText={errors}>
-        Your estimate of the total runtime of the job (in Hours).
+      {t("JobSubmitDialog.costEstimate.timeEstimateHeader")}
         <SpaceBetween direction="horizontal" size="s" key="command">
           <div style={{flexGrow: 1}}>
             <Input
@@ -131,11 +134,11 @@ function JobCostEstimate() {
               placeholder={'2.5'}
             />
           </div>
-          <Button loading={costEstimatePending} onClick={estimateCost}>Estimate</Button>
+          <Button loading={costEstimatePending} onClick={estimateCost}>{t("JobSubmitDialog.costEstimate.button")}</Button>
         </SpaceBetween>
       </FormField>
-      {costEstimate && <FormField>Estimated job cost: ${costEstimate.toFixed(2)}</FormField>}
-      {costEstimate && <pre>Price ($/h) * Time (h) * NodeCount =&gt; {priceEstimate} * {jobRuntime} * {nodes}</pre>}
+      {costEstimate && <FormField>{t("JobSubmitDialog.costEstimate.estimatedCost")} ${costEstimate.toFixed(2)}</FormField>}
+      {costEstimate && <pre>{t("JobSubmitDialog.costEstimate.formula")} =&gt; {priceEstimate} * {jobRuntime} * {nodes}</pre>}
     </div>
   </>
 }
@@ -145,14 +148,21 @@ interface JobFieldProps {
   description: string;
   placeholder: string;
   property: string;
+  disabled?: boolean;
 }
 
 function JobField({
   header,
   description,
   placeholder,
-  property
+  property,
+  disabled=false
 }: JobFieldProps) {
+
+  const setStateIfNotEmpty = React.useCallback((value: string) => {
+    !value || value === '' ? clearState([...jobPath, property]) : setState([...jobPath, property], value);
+  }, [property])
+
   return <>
     <SpaceBetween direction="vertical" size="xxs" key={property}>
       <Header 
@@ -162,9 +172,10 @@ function JobField({
       </Header>
       <FormField>
         <Input 
-          onChange={({ detail }) => {setState([...jobPath, property], detail.value);}}
+          onChange={({ detail }) => setStateIfNotEmpty(detail.value)}
           value={useState([...jobPath, property])}
           placeholder={placeholder}
+          disabled={disabled}
         />
       </FormField>
     </SpaceBetween>
@@ -174,61 +185,88 @@ function JobField({
 export default function JobSubmitDialog({
   submitCallback
 }: any) {
+  
   const { t } = useTranslation();
   const open = useState([...submitPath, 'dialog']);
-  const error = useState([...submitPath, 'error']);
+  const error = useState([...submitPath, 'error']) || "";
   const clusterName = getState(['app', 'clusters', 'selected']);
+  const clusterPath = ['clusters', 'index', clusterName];
+  const cluster = getState(clusterPath);
+  const headNode = getState([...clusterPath, 'headNode']);
 
-  const job = useState(jobPath);
+  const job: Job = useState(jobPath);
   const submitting = useState([...submitPath, 'pending']);
-  const jobType: string = useState([...submitPath, 'job-entry']) || 'command';
-
-  React.useEffect(() => setState([...jobPath, 'wrap'], true), []);
+  const jobType: string = useState([...submitPath, 'job-entry']);
+  const jobCommand: string = useState([...jobPath, 'command']);
 
   let isMemBasedSchedulingEnabled = useState(
-      ['clusters', 'index', clusterName, 'config', 'Scheduling', 'SlurmSettings', 'EnableMemoryBasedScheduling']
+    [...clusterPath, 'config', 'Scheduling', 'SlurmSettings', 'EnableMemoryBasedScheduling']
   ) || false;
 
-  const jobTypeSelect = (detail: string) => {
-    setState([...submitPath, 'job-entry'], detail)
-    detail === 'command' ? setState([...jobPath, 'wrap'], true) : clearState([...jobPath, 'wrap']);
+  let isSlurmApiEnabled = getScripts(
+    useState([...clusterPath, 'config', 'HeadNode', 'CustomActions'])
+  ).includes('slurm-rest-api');
+
+  const isScriptSelected = jobType === 'script';
+
+  const jobTypeSelect = (entryType: string) => {
+    setState([...submitPath, 'job-entry'], entryType);
+    clearState([...jobPath, 'command']);
+    entryType === 'command' ? setState([...jobPath, 'wrap'], true) : clearState([...jobPath, 'wrap']);
+    if(entryType === 'script') clearState([...jobPath, 'chdir']);
   }
 
+  const scriptSelectDescription = isSlurmApiEnabled ? "" : t("JobSubmitDialog.jobTypeScript.radioGroupDisabledDescription");
+
   const jobTypeHeader = {
-    'command': "Command",
-    'file': "Script Path"
+    'command': t("JobSubmitDialog.jobTypeCommand.header"),
+    'file': t("JobSubmitDialog.jobTypeFile.header")
   }[jobType]
 
   const jobTypeDescription = {
-    'command': "The command to run as a part of this job.",
-    'file': "Path to the script to run."
+    'command': t("JobSubmitDialog.jobTypeCommand.description"),
+    'file': t("JobSubmitDialog.jobTypeFile.description")
   }[jobType]
 
   const jobTypePlaceholder = {
     'command': "sleep 30",
-    'file': "/home/ec2-user/myscript.sbatch"
+    'file': "/home/ec2-user/myscript.sbatch",
+    'script': jobCommand || "#!/bin/bash\n"
   }[jobType]
 
-  const submitJob = () => {
-    const clusterPath = ['clusters', 'index', clusterName];
-    const cluster = getState(clusterPath);
+  const submitJob = React.useCallback(async () => {
     let user = clusterDefaultUser(cluster);
-    const headNode = getState([...clusterPath, 'headNode']);
     const success_callback = () => {
+      clearState([...submitPath, 'error']);
       setState([...submitPath, 'dialog'], false);
       setState([...submitPath, 'pending'], false);
       submitCallback && submitCallback();
     }
-    const failure_callback = (message: any) => {
-      setState([...submitPath, 'error'], message)
+    const failure_callback = (message: string) => {
+      setState([...submitPath, 'error'], message); // TODO: doesn't support translation
       setState([...submitPath, 'pending'], false);
     }
     setState([...submitPath, 'pending'], true);
-    SubmitJob(headNode.instanceId, user, job, success_callback, failure_callback)
-  };
+    const errorMessage = isScriptSelected ? 
+      await SubmitJobScript(clusterName, headNode.instanceId, user, job) 
+      : 
+      await SubmitJob(headNode.instanceId, user, job); 
+    errorMessage === "" ? success_callback() : failure_callback(errorMessage);
+    errorMessage === "" ? success_callback() : failure_callback(errorMessage);
+  }, [cluster, clusterName, headNode.instanceId, isScriptSelected, job, submitCallback]);
+
+  const submitJobIfEntered = React.useCallback(() => {
+    const entry = (getState([...jobPath, 'command']) || '').replace(/\n| /gm, '');
+    if(entry === '#!/bin/bash' || entry === '') {
+      setState([...submitPath, 'error'], t("JobSubmitDialog.errors.emptyJob"));
+    } else {
+      submitJob();
+    }
+  }, [submitJob])
 
   const cancel = () => {
-    setState([...submitPath, 'dialog'], false)
+    setState([...submitPath, 'dialog'], false);
+    clearState([...submitPath, 'error']);
   };
 
   return (
@@ -240,47 +278,48 @@ export default function JobSubmitDialog({
       footer={
         <Box float="right">
           <SpaceBetween direction="horizontal" size="xs">
-            <Button onClick={cancel}>Cancel</Button>
-            <Button loading={submitting} onClick={submitJob}>Submit</Button>
+            <Button onClick={cancel}>{t("JobSubmitDialog.cancel")}</Button>
+            <Button loading={submitting} onClick={submitJobIfEntered} variant="primary">{t("JobSubmitDialog.submit")}</Button>
           </SpaceBetween>
         </Box>
       }
-      header="Submit Job">
+      header={t("JobSubmitDialog.header")}>
       <SpaceBetween direction="vertical" size="m">
       <ColumnLayout columns={2} borders="vertical">
       
         <JobField 
-          header="Job Name" 
-          description="Please choose an identifier for this job." 
-          placeholder="job-name" 
+          header={t("JobSubmitDialog.job-name.header")}
+          description={t("JobSubmitDialog.job-name.description")}
+          placeholder={t("JobSubmitDialog.job-name.placeholder")}
           property='job-name'
         />
 
         <JobField
-          header="Working Directory" 
-          description="Please choose a working directory for the job [optional]" 
+          header={t("JobSubmitDialog.chdir.header")}
+          description={t("JobSubmitDialog.chdir.description")}
           placeholder="/home/ec2-user" 
           property='chdir'
+          disabled={isScriptSelected}
         />
 
         <JobField
-          header="Nodes"
-          description="Number of nodes for job [optional]"
+          header={t("JobSubmitDialog.nodes.header")}
+          description={t("JobSubmitDialog.nodes.description")}
           placeholder="0"
           property='nodes'
         />
 
         <JobField
-          header="Number of tasks"
-          description="Number of tasks for job [optional]"
+          header={t("JobSubmitDialog.ntasks.header")}
+          description={t("JobSubmitDialog.ntasks.description")}
           placeholder="0"
           property='ntasks'
         />
 
         { isMemBasedSchedulingEnabled && 
           <JobField
-            header={t("JobSubmitDialog.requiredMemory.header")}
-            description={t("JobSubmitDialog.requiredMemory.description")}
+            header={t("JobSubmitDialog.mem.header")}
+            description={t("JobSubmitDialog.mem.description")}
             placeholder="0"
             property='mem'
           />
@@ -292,24 +331,38 @@ export default function JobSubmitDialog({
           onChange={({detail}) => {jobTypeSelect(detail.value);}}
           value={jobType}
           items={[
-            {value: "command", label: "Run a command"},
-            {value: "file", label: "Run a script on the head node"}
+            {value: "command", label: t("JobSubmitDialog.jobTypeCommand.radioGroup")},
+            {value: "file", label: t("JobSubmitDialog.jobTypeFile.radioGroup")},
+            {value: "script", label: t("JobSubmitDialog.jobTypeScript.radioGroup"), disabled: !isSlurmApiEnabled, description: scriptSelectDescription}
           ]}
         />
-
-        <JobField
-          header={jobTypeHeader!}
-          description={jobTypeDescription!}
-          placeholder={jobTypePlaceholder!}
-          property='command'
-        />
-
       </ColumnLayout>
-      <ExpandableSection header="Cost estimate">
+
+        { isScriptSelected ? 
+          <>
+          <FileUploadButton handleData={(data: string) => setState([...jobPath, 'command'], data)}/>
+          <ConfigView 
+            config={jobTypePlaceholder!}
+            // language="sh"
+            onChange={({ detail }: any) => {
+              setState([...jobPath, 'command'], detail.value);
+            }}>
+          </ConfigView>
+          </>
+          :
+          <JobField
+            header={jobTypeHeader!}
+            description={jobTypeDescription!}
+            placeholder={jobTypePlaceholder!}
+            property='command'
+          />
+        }
+
+      <ExpandableSection header={t("JobSubmitDialog.costEstimate.header")}>
         <JobCostEstimate />
       </ExpandableSection>
       </SpaceBetween>
-      <div style={{color: 'red', marginTop: "20px"}}>{(error || "").split('\n').map((line: any, i: any) => <div key={i}>{line}</div>)}</div>
+      <div style={{color: 'red', marginTop: "20px"}}>{error.split('\n').map((line: any, i: any) => <div key={i}>{line}</div>)}</div>
     </Modal>
   );
 }
