1d78f7249cfde642d4726ead4e7d394b000f3025
Ermanno Moser <ermann@amazon.com>
2022-09-19 16:25:32 +0200

    Keep track of the OIDC provider
    This patch introduces two changes in order to keep track of which
    OIDC provider we are using:
    - We add an environmental variable with the OIDC provider the customer
      selected when deploying the Cfn template.
    - We return the value of the variable in the `get_app_config` function.

--- api/tests/test_get_app_config.py
+++ api/tests/test_get_app_config.py
@@ -2,51 +2,57 @@ from api.PclusterApiHandler import get_app_config
 
 def test_get_app_config(monkeypatch):
     """
-    Given an handler for the /get-app-config endpoint
+    Given a handler for the /get-app-config endpoint
       When authentication variables are set
-        Then it should return a the application config
+        Then it should return the application config
     """
     monkeypatch.setattr('api.PclusterApiHandler.AUTH_URL', 'some-url')
     monkeypatch.setattr('api.PclusterApiHandler.CLIENT_ID', 'some-id')
+    monkeypatch.setattr('api.PclusterApiHandler.OIDC_PROVIDER', 'some-oidc-provider')
     monkeypatch.setattr('api.PclusterApiHandler.SCOPES_LIST', 'some-scope')
     monkeypatch.setattr('api.PclusterApiHandler.SITE_URL', 'some-url')
     assert get_app_config() == {
-        "auth_url": 'some-url',
-        "client_id": 'some-id',
-        "scopes": 'some-scope openid',
-        "redirect_uri": 'some-url/login'
+        'auth_url': 'some-url',
+        'client_id': 'some-id',
+        'oidc_provider': 'some-oidc-provider',
+        'scopes': 'some-scope openid',
+        'redirect_uri': 'some-url/login'
     }
 
 def test_get_app_config_with_empty_scopes_list(monkeypatch):
     """
-    Given an handler for the /get-app-config endpoint
+    Given a handler for the /get-app-config endpoint
       When the scopes list is empty
         Then it should return openid as scope
     """
     monkeypatch.setattr('api.PclusterApiHandler.AUTH_URL', 'some-url')
     monkeypatch.setattr('api.PclusterApiHandler.CLIENT_ID', 'some-id')
+    monkeypatch.setattr('api.PclusterApiHandler.OIDC_PROVIDER', 'some-oidc-provider')
     monkeypatch.setattr('api.PclusterApiHandler.SCOPES_LIST', '')
     monkeypatch.setattr('api.PclusterApiHandler.SITE_URL', 'some-url')
     assert get_app_config() == {
-        "auth_url": 'some-url',
-        "client_id": 'some-id',
-        "scopes": 'openid',
-        "redirect_uri": 'some-url/login'
+        'auth_url': 'some-url',
+        'client_id': 'some-id',
+        'oidc_provider': 'some-oidc-provider',
+        'scopes': 'openid',
+        'redirect_uri': 'some-url/login'
     }
 
 def test_get_app_config_with_openid_as_scope(monkeypatch):
     """
-    Given an handler for the /get-app-config endpoint
+    Given a handler for the /get-app-config endpoint
       When the scopes list contains openid
-        Then it should return thes scopes list as is
+        Then it should return the scopes list as is
     """
     monkeypatch.setattr('api.PclusterApiHandler.AUTH_URL', 'some-url')
     monkeypatch.setattr('api.PclusterApiHandler.CLIENT_ID', 'some-id')
+    monkeypatch.setattr('api.PclusterApiHandler.OIDC_PROVIDER', 'some-oidc-provider')
     monkeypatch.setattr('api.PclusterApiHandler.SCOPES_LIST', 'some-scope openid')
     monkeypatch.setattr('api.PclusterApiHandler.SITE_URL', 'some-url')
     assert get_app_config() == {
-        "auth_url": 'some-url',
-        "client_id": 'some-id',
-        "scopes": 'some-scope openid',
-        "redirect_uri": 'some-url/login'
+        'auth_url': 'some-url',
+        'client_id': 'some-id',
+        'oidc_provider': 'some-oidc-provider',
+        'scopes': 'some-scope openid',
+        'redirect_uri': 'some-url/login'
     }
