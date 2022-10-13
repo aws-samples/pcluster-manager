559a40e4d0711b24fd5f6d8c78213f49b0785c28
Mattia Franchetto <mt.franchetto@gmail.com>
2022-10-13 14:39:17 +0200

    Production deploy workflow (#317)
    * Add CFN template to match Github and production account
    
    * Add release workflow
    
    * Fix region of prod workflow
    
    * Fix missing IAM permissions
    
    * Fix release scripts calls
    
    * Fix scripts paths
    
    * Remove sed backup logic
    
    * Remove ACLs from s3 cp
    
    * Change production workflow trigger
    
    * Remove not needed IAM permissions from s3 access

--- infrastructure/release_infrastructure.sh
+++ infrastructure/release_infrastructure.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
@@ -10,11 +11,11 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-source common.sh
-source bucket_configuration.sh
-trap 'error' ERR
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+source ${SCRIPT_DIR}/common.sh
+source ${SCRIPT_DIR}/bucket_configuration.sh
+trap 'error' ERR
 
 echo "Uploading the main templates"
 "${SCRIPT_DIR}"/upload.sh "$SCRIPT_DIR"
