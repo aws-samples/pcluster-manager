17eecb34582157c0c44f31aecbecbf6a9e91e6ca
Tommaso Scarlatti <tsscarla@amazon.com>
2022-10-18 23:51:12 +0200

    Add SlurmMemorySettings component

---
+++ frontend/src/old-pages/Configure/SlurmMemorySettings.tsx
@@ -0,0 +1,110 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+// with the License. A copy of the License is located at
+//
+// http://aws.amazon.com/apache2.0/
+//
+// or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+// OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as React from 'react'
+import {Trans, useTranslation} from 'react-i18next'
+import {
+  Container,
+  Header,
+  Alert,
+  Toggle,
+  SpaceBetween,
+} from '@awsui/components-react'
+import {setState, getState, useState, clearState} from '../../store'
+
+function SlurmMemorySettings() {
+  const {t} = useTranslation()
+  const [infoAlertVisible, setInfoAlertVisible] = React.useState(false)
+  const slurmSettingsPath = [
+    'app',
+    'wizard',
+    'config',
+    'Scheduling',
+    'SlurmSettings',
+  ]
+  const memoryBasedSchedulingEnabledPath = [
+    ...slurmSettingsPath,
+    'EnableMemoryBasedScheduling',
+  ]
+  const memoryBasedSchedulingEnabled = useState(
+    memoryBasedSchedulingEnabledPath,
+  )
+
+  const toggleMemoryBasedSchedulingEnabled = () => {
+    const setMemoryBasedSchedulingEnabled = !memoryBasedSchedulingEnabled
+    if (setMemoryBasedSchedulingEnabled)
+      setState(
+        memoryBasedSchedulingEnabledPath,
+        setMemoryBasedSchedulingEnabled,
+      )
+    else {
+      clearState(memoryBasedSchedulingEnabledPath)
+      if (Object.keys(getState([...slurmSettingsPath])).length === 0)
+        clearState([...slurmSettingsPath])
+    }
+  }
+
+  return (
+    <Container
+      header={
+        <Header variant="h2">
+          <SpaceBetween size="xs" direction="horizontal">
+            {t('wizard.queues.slurmMemorySettings.container.title')}
+            <a
+              href="#"
+              onClick={() => setInfoAlertVisible(true)}
+              style={{
+                fontWeight: '700',
+                fontStyle: 'normal',
+                fontSize: '12px',
+                color: '#0073BB',
+              }}
+            >
+              {t('wizard.queues.slurmMemorySettings.container.info')}
+            </a>
+          </SpaceBetween>
+        </Header>
+      }
+    >
+      <SpaceBetween size={'s'} direction={'vertical'}>
+        <div
+          key="memory-based-scheduling-enabled"
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Toggle
+            checked={memoryBasedSchedulingEnabled}
+            onChange={toggleMemoryBasedSchedulingEnabled}
+          >
+            <Trans i18nKey="wizard.queues.slurmMemorySettings.toggle.label" />
+          </Toggle>
+        </div>
+        <Alert
+          onDismiss={() => setInfoAlertVisible(false)}
+          visible={infoAlertVisible}
+          dismissAriaLabel={t(
+            'wizard.queues.slurmMemorySettings.info.dismissAriaLabel',
+          )}
+          dismissible
+          header={t('wizard.queues.slurmMemorySettings.info.header')}
+        >
+          {t('wizard.queues.slurmMemorySettings.info.body')}
+        </Alert>
+      </SpaceBetween>
+    </Container>
+  )
+}
+
+export {SlurmMemorySettings}
