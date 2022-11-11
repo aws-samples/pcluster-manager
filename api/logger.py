cbd884bfa7a0ca899b6514f2d2fc8e0ca60ef1c4
Tommaso Scarlatti <tsscarla@amazon.com>
2022-11-11 11:33:11 +0100

    Introduce logger for Python backend

---
+++ api/logger.py
@@ -0,0 +1,35 @@
+import logging
+
+
+class DefaultLogger(object):
+  def __init__(self, is_running_local):
+    self.logger = logging.getLogger("pcluster-manager")
+    if is_running_local:
+      handler = logging.StreamHandler()
+      handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+      self.logger.addHandler(handler)
+      self.logger.setLevel(logging.DEBUG)
+    else:
+      self.logger.setLevel(logging.INFO)
+
+  def _log_output(self, msg, extra):
+    _extra = {} if extra is None else extra
+    _extra["message"] = msg
+    return _extra
+
+  def debug(self, msg, extra=None):
+    self.logger.debug(self._log_output(msg, extra)) 
+
+  def info(self, msg, extra=None):
+    self.logger.info(self._log_output(msg, extra))  
+
+  def warning(self, msg, extra=None):
+    self.logger.warning(self._log_output(msg, extra))   
+    
+  def error(self, msg, extra=None):
+    self.logger.error(self._log_output(msg, extra))
+
+  def critical(self, msg, extra=None):
+    self.logger.critical(self._log_output(msg, extra))
+
+
