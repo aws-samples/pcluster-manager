7c20f17801bcb4f5497adca7b6324366c70920a7
Tommaso Scarlatti <tsscarla@amazon.com>
2022-11-11 11:33:11 +0100

    Introduce logger for Python backend

---
+++ api/tests/test_logger.py
@@ -0,0 +1,25 @@
+from api.logger import DefaultLogger
+import logging
+
+
+def test_logger_level_prod():
+  """
+  Given a DefaultLogger
+    When not running local
+      It should set log level to INFO
+  """
+
+  logger = DefaultLogger(is_running_local=False)
+  assert logger.logger.getEffectiveLevel() == logging.INFO
+
+
+def test_logger_level_dev():
+  """
+  Given a DefaultLogger
+    When is running local
+      It should set log level to DEBUG
+  """
+
+  logger = DefaultLogger(is_running_local=True)
+  assert logger.logger.getEffectiveLevel() == logging.DEBUG
+
