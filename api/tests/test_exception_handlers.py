35adb12195d0417de1344cd7ca76a1755b87c989
Marco Basile <mrbarco@amazon.com>
2022-11-17 12:49:58 +0100

    moved tests to common folder

--- api/tests/test_exception_handlers.py
+++ api/tests/test_exception_handlers.py
@@ -1,5 +1,13 @@
+import pytest
+from botocore.exceptions import ClientError
+
 import app
 
+@pytest.fixture()
+def client_error_response():
+    error_response = dict(Error={'Code': 400, 'Message': 'Operation failed'})
+    error_response['ResponseMetadata'] = dict(HTTPStatusCode=400)
+    return ClientError(error_response, 'failed_operation')
 
 def test_boto3_exception_handler(client, client_error_response, monkeypatch):
     def set_user_role_raising_clienterror():
