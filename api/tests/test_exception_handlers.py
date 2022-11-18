a379d18a0cf1c2b18c64656775c7cf0fc64e8b6f
Marco Basile <mrbarco@amazon.com>
2022-11-18 18:07:12 +0100

    Added test for newly implemented ValueError exception handler

--- api/tests/test_exception_handlers.py
+++ api/tests/test_exception_handlers.py
@@ -19,6 +19,15 @@ def test_boto3_exception_handler(client, client_error_response, app, monkeypatch
     assert response.status_code == 400
     assert response.json == {'error': {'Code': 400, 'Message': 'Operation failed'}}
 
+def test_value_error_exception_handler(client, app, monkeypatch):
+    def push_log_raising():
+        raise ValueError('Validation error')
+
+    monkeypatch.setitem(app.view_functions, 'push_log', push_log_raising)
+    response = client.post('/push-log')
+
+    assert response.status_code == 400
+    assert response.json == {'error': {'Code': 400, 'Message': 'Validation error'}}
 
 def test_global_exception_handler_with_app_logic(client, app, monkeypatch):
     def get_app_config_raising_generic_exception():
