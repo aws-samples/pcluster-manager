0d64e14bbf5df13d1750a1b17f14722eca90a095
Marco Basile <mrbarco@amazon.com>
2022-11-18 10:45:42 +0100

    Adjusted tests to new exception handler + improved improved new tests

--- api/tests/test_exception_handlers.py
+++ api/tests/test_exception_handlers.py
@@ -1,7 +1,6 @@
 import pytest
 from botocore.exceptions import ClientError
 
-import app
 
 @pytest.fixture()
 def client_error_response():
@@ -9,25 +8,23 @@ def client_error_response():
     error_response['ResponseMetadata'] = dict(HTTPStatusCode=400)
     return ClientError(error_response, 'failed_operation')
 
-def test_boto3_exception_handler(client, client_error_response, monkeypatch):
+
+def test_boto3_exception_handler(client, client_error_response, app, monkeypatch):
     def set_user_role_raising_clienterror():
         raise client_error_response
 
-    monkeypatch.setattr(app, 'set_user_role', set_user_role_raising_clienterror)
-    monkeypatch.setenv('ENABLE_AUTH', 'false')
-
+    monkeypatch.setitem(app.view_functions, 'set_user_role_', set_user_role_raising_clienterror)
     response = client.put('/manager/set_user_role', json={'username': 'some-user', 'role': 'some-role'})
 
     assert response.status_code == 400
     assert response.json == {'error': {'Code': 400, 'Message': 'Operation failed'}}
 
 
-def test_global_exception_handler_with_app_logic(client, monkeypatch):
-    def set_user_role_raising_generic_exception():
+def test_global_exception_handler_with_app_logic(client, app, monkeypatch):
+    def get_app_config_raising_generic_exception():
         raise Exception('generic exception')
 
-    monkeypatch.setattr(app, 'get_app_config', set_user_role_raising_generic_exception)
-
+    monkeypatch.setitem(app.view_functions, 'get_app_config_', get_app_config_raising_generic_exception)
     response = client.get('/manager/get_app_config')
 
     assert response.status_code == 500
