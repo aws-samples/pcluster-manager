5851e7f7e75e009ce54597236a245a89598bb5e9
Barco <BarcoMasile@users.noreply.github.com>
2022-11-21 21:18:36 +0100

    Added public endpoint to push log entry to from the frontend (#369)
    * first implementation
    
    * Added ValueError handler
    
    * Implemented full validation and tests
    
    * Added test for newly implemented ValueError exception handler
    
    * Corrected typo
    
    * Adjusted test string
    
    * Added `source` parameter to the logs from the frontend
    
    * Changed endpoint string + added authentication to controller + adjusted tests
    
    Co-authored-by: Marco Basile <mrbarco@amazon.com>

--- api/exception/__init__.py
+++ api/exception/__init__.py
@@ -2,7 +2,8 @@ from boto3.exceptions import Boto3Error
 from botocore.exceptions import ClientError
 from werkzeug.routing import WebsocketMismatch
 
-from .handlers import global_exception_handler, boto3_exception_handler, websocket_mismatch_nop_handler
+from .handlers import global_exception_handler, boto3_exception_handler, websocket_mismatch_nop_handler, \
+    value_error_handler
 
 
 class ExceptionHandler(object):
@@ -16,6 +17,7 @@ class ExceptionHandler(object):
         app.register_error_handler(Boto3Error, boto3_exception_handler)
         app.register_error_handler(ClientError, boto3_exception_handler)
 
+        app.register_error_handler(ValueError, value_error_handler)
         app.register_error_handler(Exception, global_exception_handler)
 
         # in local dev, handle specific Exception caused by HMR requests from FE
---
+++ api/tests/test_push_log.py
@@ -0,0 +1,90 @@
+from os import environ
+
+import pytest
+
+
+def trim_log(caplog):
+    return caplog.text.replace('\n', '')
+
+@pytest.fixture(scope='function')
+def disable_auth(monkeypatch):
+    monkeypatch.setitem(environ, 'ENABLE_AUTH', 'false')
+
+
+def test_push_log_controller_with_valid_json_no_extra(client, caplog, disable_auth):
+    request_body = {'message': 'sample-message', 'level': 'info'}
+    expected_log = "INFO     pcluster-manager:logger.py:24 {'source': 'frontend', 'message': 'sample-message'}"
+
+    caplog.clear()
+    response = client.post('/logs', json=request_body)
+
+    assert response.status_code == 200
+    assert trim_log(caplog) == expected_log
+
+
+def test_push_log_controller_with_valid_json_with_extra(client, caplog, disable_auth):
+    request_body = {'message': 'sample-message', 'level': 'error',
+                    'extra': {'extra_1': 'value_1', 'extra_2': 'value_2'}}
+    expected_log = "ERROR    pcluster-manager:logger.py:30 {'extra_1': 'value_1', 'extra_2': 'value_2', 'source': 'frontend', 'message': 'sample-message'}"
+
+    caplog.clear()
+    response = client.post('/logs', json=request_body)
+
+    assert response.status_code == 200
+    assert trim_log(caplog) == expected_log
+
+
+def test_push_log_controller_with_invalid_message_key(client, disable_auth):
+    request_body = {'message_wrong': 'sample-message', 'level': 'info'}
+
+    response = client.post('/logs', json=request_body)
+
+    assert response.status_code == 400
+    assert response.json == {
+        'error': {
+            'Code': 400,
+            'Message': 'Request body missing one or more mandatory fields ["message", "level"]'
+        }
+    }
+
+
+def test_push_log_controller_with_invalid_level_key(client, disable_auth):
+    request_body = {'message': 'sample-message', 'level_wrong': 'info'}
+
+    response = client.post('/logs', json=request_body)
+
+    assert response.status_code == 400
+    assert response.json == {
+        'error': {
+            'Code': 400,
+            'Message': 'Request body missing one or more mandatory fields ["message", "level"]'
+        }
+    }
+
+
+def test_push_log_controller_with_invalid_level_value(client, disable_auth):
+    request_body = {'message': 'sample-message', 'level': 'wrong-level'}
+
+    response = client.post('/logs', json=request_body)
+
+    assert response.status_code == 400
+    assert response.json == {
+        'error': {
+            'Code': 400,
+            'Message': 'Invalid logging level requested'
+        }
+    }
+
+
+def test_push_log_controller_with_invalid_extra_value(client, disable_auth):
+    request_body = {'message': 'sample-message', 'level': 'info', 'extra': 'wrong-value'}
+
+    response = client.post('/logs', json=request_body)
+
+    assert response.status_code == 400
+    assert response.json == {
+        'error': {
+            'Code': 400,
+            'Message': 'Extra param must be a valid json object'
+        }
+    }
