e37d86f48daa2a09524b585c19893ae5a107ee31
Alessandro Menduni <mendaomn@amazon.com>
2022-11-23 11:43:54 +0100

    fix test

--- frontend/src/components/__tests__/ErrorBoundary.test.tsx
+++ frontend/src/components/__tests__/ErrorBoundary.test.tsx
@@ -1,35 +1,65 @@
 import {render, waitFor} from '@testing-library/react'
 import ErrorBoundary from '../ErrorBoundary'
+import i18n from 'i18next'
+import {I18nextProvider, initReactI18next} from 'react-i18next'
+
+i18n.use(initReactI18next).init({
+  resources: {},
+  lng: 'en',
+})
+
+const MockProviders = (props: any) => (
+  <I18nextProvider i18n={i18n}>{props.children}</I18nextProvider>
+)
 
 const Child = () => {
   throw new Error()
 }
-
-const mockAddEventListener = jest.fn()
-window.addEventListener = mockAddEventListener
 
 describe('Given an ErrorBoundary component', () => {
   describe('when there is an error in a Child component', () => {
     it('should render the fallback ui', async () => {
       const {getByText} = await waitFor(() =>
         render(
-          <ErrorBoundary>
-            <Child />
-          </ErrorBoundary>,
+          <MockProviders>
+            <ErrorBoundary>
+              <Child />
+            </ErrorBoundary>
+          </MockProviders>,
         ),
       )
       expect(getByText('errorBoundary.modal.header')).toBeTruthy()
     })
   })
 
-  describe.only('when the component is mounted', () => {
-    beforeEach(() => {
-      render(<ErrorBoundary>test</ErrorBoundary>)
-      window.dispatchEvent(new Event('unhandledrejection'))
+  describe('when the component is mounted', () => {
+    let mockWindowObject: any
+
+    beforeEach(async () => {
+      mockWindowObject = {
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+      }
+
+      render(
+        <MockProviders>
+          <ErrorBoundary windowObject={mockWindowObject}>
+            some content
+          </ErrorBoundary>
+        </MockProviders>,
+      )
     })
+
     it('should listen for unhandledrejection events', () => {
-      expect(mockAddEventListener).toHaveBeenCalledWith(
+      expect(mockWindowObject.addEventListener).toHaveBeenCalledTimes(2)
+      expect(mockWindowObject.addEventListener).toHaveBeenNthCalledWith(
+        1,
         'unhandledrejection',
+        expect.any(Function),
+      )
+      expect(mockWindowObject.addEventListener).toHaveBeenNthCalledWith(
+        2,
+        'error',
         expect.any(Function),
       )
     })
