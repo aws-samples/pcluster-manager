580dff6a1b505fe61c103993d617e0521e8fce67
Marco Basile <mrbarco@amazon.com>
2022-11-24 17:53:35 +0100

    refactored unit tests

--- frontend/src/logger/__tests__/logger.test.ts
+++ frontend/src/logger/__tests__/logger.test.ts
@@ -1,33 +1,29 @@
 import {AppConfig} from '../../app-config/types'
-import {Logger} from '../logger'
-import axios from 'axios'
+import {Logger} from '../RemoteLogger'
 
-const mockedAxios = axios as jest.Mocked<typeof axios>
-jest.mock('axios')
+const mockedExecuteRequest = jest.fn()
 
 describe('given the application config', () => {
-  let mockAppConfig: AppConfig
+  let mockAppConfig: AppConfig = {
+    authUrl: 'http://somepath.com',
+    clientId: 'some-id',
+    redirectUri: 'some-uri',
+    scopes: 'some-list',
+  }
 
   beforeEach(() => {
-    mockAppConfig = {
-      authUrl: 'http://somepath.com',
-      clientId: 'some-id',
-      redirectUri: 'some-uri',
-      scopes: 'some-list',
-    }
-
-    mockedAxios.post.mockClear()
+    mockedExecuteRequest.mockClear()
   })
 
   describe('and an axios instance', () => {
     const response = {status: 200}
-    mockedAxios.post.mockResolvedValue(response)
+    mockedExecuteRequest.mockResolvedValue(response)
 
     describe('and a PCM Logger instance', () => {
-      let logger = new Logger(mockedAxios, mockAppConfig)
+      let logger = new Logger(mockedExecuteRequest, mockAppConfig)
 
+      const expectedMethod = 'post'
       const expectedPath = '/logs'
-      const expectedConfig = {headers: {'Content-Type': 'application/json'}}
 
       it('should successfully push a log entry', async () => {
         const response = await logger.info('info message')
@@ -36,12 +32,12 @@ describe('given the application config', () => {
           message: 'info message',
           level: 'INFO',
         }
-
-        expect(mockedAxios.post).toHaveBeenCalledTimes(1)
-        expect(mockedAxios.post).toHaveBeenCalledWith(
+        expect(mockedExecuteRequest).toHaveBeenCalledTimes(1)
+        expect(mockedExecuteRequest).toHaveBeenCalledWith(
+          expectedMethod,
           expectedPath,
           expectedLogEntry,
-          expectedConfig,
+          mockAppConfig,
         )
         expect(response.status).toBe(200)
       })
@@ -61,29 +57,29 @@ describe('given the application config', () => {
           },
         }
 
-        expect(mockedAxios.post).toHaveBeenCalledTimes(1)
-        expect(mockedAxios.post).toHaveBeenCalledWith(
+        expect(mockedExecuteRequest).toHaveBeenCalledTimes(1)
+        expect(mockedExecuteRequest).toHaveBeenCalledWith(
+          expectedMethod,
           expectedPath,
           expectedLogEntry,
-          expectedConfig,
+          mockAppConfig,
         )
         expect(response.status).toBe(200)
       })
 
-      it('logs an error to stderr when incorrect data is passed', async () => {
+      it('returns an error when incorrect data is passed', async () => {
         const logSpy = jest.spyOn(console, 'warn')
-        mockedAxios.post.mockRejectedValueOnce({
+        mockedExecuteRequest.mockRejectedValueOnce({
           Code: 400,
           Message: 'Error message',
         })
 
-        const response = await logger.error('error message', {
+        await logger.error('error message', {
           extra1: 'value1',
           extra2: 'value2',
         })
 
-        expect(response.status).toBe(400)
-        expect(response.message).toBe('Error message')
+        expect(logSpy).toHaveBeenCalledWith('Unable to push log entry')
       })
     })
   })
