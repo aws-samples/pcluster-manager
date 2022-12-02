6128d48bd104733e88b4e571ed05885b64b58d93
Marco Basile <mrbarco@amazon.com>
2022-12-02 10:54:45 +0100

    Improved tests after comments in PR

--- api/tests/test_add_response_headers.py
+++ api/tests/test_add_response_headers.py
@@ -1,6 +1,3 @@
-from api.security.headers import SECURITY_HEADERS, CORS_HEADERS
-
-
 def test_response_security_headers_dev(dev_app, dev_client):
     """
     Given a PCM app in local development
@@ -8,9 +5,12 @@ def test_response_security_headers_dev(dev_app, dev_client):
         Then it should have security headers correctly set for local development
     """
     response = dev_client.get('/manager/get_app_config')
-
-    for header in SECURITY_HEADERS:
-        assert header['key'] in response.headers
+    expected_security_headers = [
+        'X-Frame-Options', 'X-Content-Type-Options', 'Referrer-Policy',
+        'Strict-Transport-Security', 'Permissions-Policy', 'X-XSS-Protection'
+    ]
+    for header in expected_security_headers:
+        assert header in response.headers
 
 
 def test_response_security_headers_prod(app, client):
@@ -20,6 +20,10 @@ def test_response_security_headers_prod(app, client):
         Then it should have security headers correctly set
     """
     response = client.get('/manager/get_app_config')
-
-    for header in [*CORS_HEADERS, *SECURITY_HEADERS]:
-        assert header['key'] in response.headers
+    expected_security_headers = [
+        'Cross-Origin-Resource-Policy', 'Cross-Origin-Embedder-Policy',
+        'X-Frame-Options', 'X-Content-Type-Options', 'Referrer-Policy',
+        'Strict-Transport-Security', 'Permissions-Policy', 'X-XSS-Protection'
+    ]
+    for header in expected_security_headers:
+        assert header in response.headers
