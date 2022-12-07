dd233422aeaef1a2dace7bb247ca2f36130910eb
Marco Basile <mrbarco@amazon.com>
2022-12-07 13:10:07 +0100

    refactor: move ExceptionHandler to handers.py

--- api/exception/__init__.py
+++ api/exception/__init__.py
@@ -1,25 +1,2 @@
-from boto3.exceptions import Boto3Error
-from botocore.exceptions import ClientError
-from werkzeug.routing import WebsocketMismatch
-
-from .handlers import global_exception_handler, boto3_exception_handler, websocket_mismatch_nop_handler, \
-    value_error_handler
-
-
-class ExceptionHandler(object):
-
-    def __init__(self, app=None, running_local=False):
-        self.running_local = running_local
-        if app is not None:
-            self.init_app(app)
-
-    def init_app(self, app):
-        app.register_error_handler(Boto3Error, boto3_exception_handler)
-        app.register_error_handler(ClientError, boto3_exception_handler)
-
-        app.register_error_handler(ValueError, value_error_handler)
-        app.register_error_handler(Exception, global_exception_handler)
-
-        # in local dev, handle specific Exception caused by HMR requests from FE
-        if self.running_local:
-            app.register_error_handler(WebsocketMismatch, websocket_mismatch_nop_handler)
+from .handlers import ExceptionHandler
+from .exceptions import *
--- api/exception/handlers.py
+++ api/exception/handlers.py
@@ -1,5 +1,7 @@
-
+from boto3.exceptions import Boto3Error
+from botocore.exceptions import ClientError
 from flask import jsonify
+from werkzeug.routing import WebsocketMismatch
 
 from api.pcm_globals import logger
 
@@ -34,3 +36,22 @@ def global_exception_handler(err):
 def __handler_response(code, description='Something went wrong'):
     response = {'Code': code, 'Message': description}
     return jsonify(error=response), code
+
+
+class ExceptionHandler(object):
+
+    def __init__(self, app=None, running_local=False):
+        self.running_local = running_local
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.register_error_handler(Boto3Error, boto3_exception_handler)
+        app.register_error_handler(ClientError, boto3_exception_handler)
+
+        app.register_error_handler(ValueError, value_error_handler)
+        app.register_error_handler(Exception, global_exception_handler)
+
+        # in local dev, handle specific Exception caused by HMR requests from FE
+        if self.running_local:
+            app.register_error_handler(WebsocketMismatch, websocket_mismatch_nop_handler)
