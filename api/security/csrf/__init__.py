f48c29e2c19c0c941000756a762391ffa3b3e2c8
Marco Basile <mrbarco@amazon.com>
2022-12-08 22:51:24 +0100

    csrf implementation

---
+++ api/security/csrf/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask, Blueprint, current_app, jsonify
+
+from api.security.csrf.constants import CSRF_SECRET_KEY, SALT
+from api.security.csrf.csrf import generate_csrf_token, set_csrf_cookie
+
+from api.security.fingerprint import IFingerprintGenerator
+
+csrf_blueprint = Blueprint('csrf', __name__)
+
+
+@csrf_blueprint.get('/csrf')
+def get_and_set_csrf_token():
+    csrf_secret_key = current_app.config.get(CSRF_SECRET_KEY)
+    csrf_token = generate_csrf_token(csrf_secret_key, SALT)
+
+    resp = jsonify(csrf_token=csrf_token)
+    set_csrf_cookie(resp, csrf_token)
+    return resp
+
+
+class CSRF(object):
+
+    def __init__(self, app: Flask = None, fingerprint_generator=None):
+        if app is not None and fingerprint_generator is not None:
+            self.init_app(app, fingerprint_generator)
+
+    def init_app(self, app, fingerprint_generator: IFingerprintGenerator):
+        csrf_secret_key = fingerprint_generator.fingerprint()
+
+        app.config['CSRF_SECRET_KEY'] = csrf_secret_key
+        app.register_blueprint(csrf_blueprint)
---
+++ api/security/csrf/csrf.py
@@ -0,0 +1,57 @@
+import functools
+import hashlib
+import os
+
+from flask import request, current_app
+from itsdangerous import URLSafeSerializer
+
+from api.exception import CSRFError
+from api.security.csrf import CSRF_SECRET_KEY
+from api.security.csrf.constants import CSRF_COOKIE_NAME, SALT, CSRF_TOKEN_HEADER
+
+
+def generate_csrf_token(secret_key, salt):
+    serializer = URLSafeSerializer(secret_key, salt)
+    csrf_token_value = hashlib.sha256(os.urandom(64)).hexdigest()
+    csrf_token = serializer.dumps(csrf_token_value)
+    return csrf_token
+
+
+def parse_csrf_token(secret_key, salt, token):
+    serializer = URLSafeSerializer(secret_key, salt)
+    try:
+        return serializer.loads(token)
+    except Exception as ex:
+        raise CSRFError(str(ex))
+
+
+def validate_csrf(secret_key, csrf_cookie, csrf_header):
+    csrf_cookie = parse_csrf_token(secret_key, SALT, csrf_cookie)
+    csrf_header = parse_csrf_token(secret_key, SALT, csrf_header)
+
+    return csrf_cookie == csrf_header
+
+
+def set_csrf_cookie(resp, csrf_token):
+    resp.set_cookie(CSRF_COOKIE_NAME, value=csrf_token)
+
+
+def csrf_needed(func):
+    @functools.wraps(func)
+    def csrf_cookie_protect(*args, **kwargs):
+        csrf_cookie = request.cookies.get(CSRF_COOKIE_NAME)
+        csrf_header = request.headers.get(CSRF_TOKEN_HEADER)
+        secret_key = current_app.config[CSRF_SECRET_KEY]
+
+        if not csrf_cookie:
+            raise ValueError('Missing CSRF cookie')
+
+        if not csrf_header:
+            raise ValueError('Missing CSRF header in request')
+
+        if validate_csrf(secret_key, csrf_cookie, csrf_header):
+            return func(*args, **kwargs)
+        else:
+            raise CSRFError("provided CSRF value does not match")
+
+    return csrf_cookie_protect
