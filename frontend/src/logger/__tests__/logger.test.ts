80de61617f9bf0989b531f0fc2e873d6703bae17
Mattia Franchetto <mtfranco@amazon.it>
2022-12-29 13:28:10 +0100

    Add buffering to logger

--- frontend/src/logger/__tests__/logger.test.ts
+++ frontend/src/logger/__tests__/logger.test.ts
@@ -1,3 +1,4 @@
+import {ILogger} from '../ILogger'
 import {Logger} from '../RemoteLogger'
 
 const mockedExecuteRequest = jest.fn()
@@ -5,19 +6,20 @@ const mockedExecuteRequest = jest.fn()
 describe('Given a RemoteLogger', () => {
   beforeEach(() => {
     mockedExecuteRequest.mockClear()
+    mockedExecuteRequest.mockResolvedValue({status: 200})
   })
 
   describe('when buffering is not enabled', () => {
-    const response = {status: 200}
-    mockedExecuteRequest.mockResolvedValue(response)
-
-    let logger = new Logger(mockedExecuteRequest)
+    let logger: ILogger
+    beforeEach(() => {
+      logger = new Logger(mockedExecuteRequest, {size: 1, window: 0})
+    })
 
     const expectedMethod = 'post'
     const expectedPath = '/logs'
 
-    it('should successfully push a log entry', async () => {
-      const response = await logger.info('info message')
+    it('should successfully push a log entry', () => {
+      logger.info('info message')
 
       const expectedLogEntry = {
         logs: [
@@ -36,11 +38,10 @@ describe('Given a RemoteLogger', () => {
         expectedPath,
         expectedLogEntry,
       )
-      expect(response.status).toBe(200)
     })
 
-    it('should successfully push a log entry with extra parameters', async () => {
-      const response = await logger.error('error message', {
+    it('should successfully push a log entry with extra parameters', () => {
+      logger.error('error message', {
         extra1: 'value1',
         extra2: 'value2',
       })
@@ -65,11 +66,10 @@ describe('Given a RemoteLogger', () => {
         expectedPath,
         expectedLogEntry,
       )
-      expect(response.status).toBe(200)
     })
 
-    it('should successfully push a log entry with a `source` parameter', async () => {
-      await logger.error(
+    it('should successfully push a log entry with a `source` parameter', () => {
+      logger.error(
         'error message',
         {
           extra1: 'value1',
@@ -99,21 +99,82 @@ describe('Given a RemoteLogger', () => {
         expectedLogEntry,
       )
     })
+  })
 
-    it('logs a warning about the impossiblity to push the log entry', async () => {
-      const logSpy = jest.spyOn(console, 'warn')
-      mockedExecuteRequest.mockRejectedValueOnce({
-        Code: 400,
-        Message: 'Error message',
+  describe('when buffering is enabled', () => {
+    let subject: ILogger
+    beforeEach(() => {
+      jest.useFakeTimers()
+      subject = new Logger(mockedExecuteRequest, {size: 2, window: 1 * 1000})
+    })
+    afterEach(() => jest.useRealTimers())
+    describe('when the items logged are less than the buffer size', () => {
+      it('should not send the logs', () => {
+        subject.info('Test log')
+
+        expect(mockedExecuteRequest).not.toHaveBeenCalled()
       })
+    })
 
-      await logger.error('error message', {
-        extra1: 'value1',
-        extra2: 'value2',
-        source: 'frontend',
+    describe('when the items logged are more than the buffer size', () => {
+      it('should flush the buffer', () => {
+        subject.info('Test log')
+        subject.warning('Warning log')
+
+        expect(mockedExecuteRequest).toHaveBeenCalledWith('post', '/logs', {
+          logs: [
+            expect.objectContaining({message: 'Test log'}),
+            expect.objectContaining({message: 'Warning log'}),
+          ],
+        })
       })
+    })
 
-      expect(logSpy).toHaveBeenCalledWith('Unable to push log entry')
+    describe('when the items logged are less than the buffer size but the window time has passed', () => {
+      it('should flush the buffer', () => {
+        subject.info('Test log')
+        jest.advanceTimersByTime(1100)
+
+        expect(mockedExecuteRequest).toHaveBeenCalledWith('post', '/logs', {
+          logs: [expect.objectContaining({message: 'Test log'})],
+        })
+      })
+    })
+
+    describe('when the window time has passed but the buffer is empty', () => {
+      it('should not perform any calls', () => {
+        subject.info('Test log')
+        subject.warning('Warning log')
+        jest.advanceTimersByTime(1100)
+
+        expect(mockedExecuteRequest).toHaveBeenCalledTimes(1)
+      })
+    })
+
+    describe('when the logging call fails', () => {
+      beforeEach(() => {
+        jest.useRealTimers()
+        mockedExecuteRequest.mockReset()
+        mockedExecuteRequest.mockRejectedValue({
+          Code: 400,
+          Message: 'Error message',
+        })
+      })
+      it('should try to send logs at the next log call', async () => {
+        subject.info('Test log')
+        subject.warning('Warning log')
+        await new Promise(resolve => setTimeout(resolve, 1))
+        subject.info('New log')
+
+        expect(mockedExecuteRequest).toHaveBeenCalledTimes(2)
+        expect(mockedExecuteRequest).toHaveBeenCalledWith('post', '/logs', {
+          logs: [
+            expect.objectContaining({message: 'Test log'}),
+            expect.objectContaining({message: 'Warning log'}),
+            expect.objectContaining({message: 'New log'}),
+          ],
+        })
+      })
     })
   })
 })
