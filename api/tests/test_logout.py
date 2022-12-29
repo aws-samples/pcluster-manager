1c3c4246d3f52b6f78fc95104ef7808b0ebcff34
Marco Basile <mrbarco@amazon.com>
2022-12-29 12:40:28 +0100

    adjusted logout tests

--- api/tests/test_logout.py
+++ api/tests/test_logout.py
@@ -1,18 +1,29 @@
 import os
 from unittest import mock
+
+import pytest
+from werkzeug.utils import redirect
+
 from api.PclusterApiHandler import logout
 
+@pytest.fixture
+def mock_cognito_redirect(mocker):
+    mocked_cognito_redirect_url = 'some-url/logout?client_id=client_id&redirect_uri=redirect_uri&response_type=code&scope=scope_list'
+    mocked_redirect = redirect(mocked_cognito_redirect_url, code=302)
+    mocker.patch('api.PclusterApiHandler.__cognito_logout_redirect', return_value=mocked_redirect)
 
-def test_logout_redirect():
+    return mocked_cognito_redirect_url
+
+def test_logout_redirect(mock_cognito_redirect):
     """
-    Given an handler for the /logout endpoint
+    Given a handler for the /logout endpoint
       When user logs out
         Then it should redirect to index.html
     """
     res = logout()
 
     assert res.status_code == 302
-    assert res.location == '/index.html'
+    assert res.location == mock_cognito_redirect
 
 def test_logout_clear_cookies(mocker, app):
     """
@@ -26,3 +37,4 @@ def test_logout_clear_cookies(mocker, app):
     assert "accessToken=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/" in cookie_list
     assert "idToken=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/" in cookie_list
     assert "refreshToken=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/" in cookie_list
+    assert "csrf=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/" in cookie_list
