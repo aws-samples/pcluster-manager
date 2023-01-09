f84985b2a2505fb58917c51d179035cb6bbc9621
Barco <BarcoMasile@users.noreply.github.com>
2023-01-09 18:43:06 +0100

    Added refresh token revocation towards Cognito when logging out (#467)
    * implemented refresh token revocation towards cognito
    * adjusted tests
    * adjusted tests + added new test
    
    Co-authored-by: Marco Basile <mrbarco@amazon.com>

---
+++ api/tests/test_revoke_cognito_refresh_token.py
@@ -0,0 +1,37 @@
+from unittest.mock import call, ANY
+
+import pytest
+
+from api.PclusterApiHandler import revoke_cognito_refresh_token
+from api.logging import DefaultLogger
+
+
+@pytest.fixture
+def mock_requests(mocker):
+    return mocker.patch('api.PclusterApiHandler.requests')
+
+@pytest.fixture
+def mock_logger(mocker):
+    return mocker.patch('api.PclusterApiHandler.logger', DefaultLogger(is_running_local=False))
+
+class MockResponse:
+    def __init__(self, status_code):
+        self.status_code = status_code
+
+def test_revoke_cognito_refresh_token_success(mock_requests):
+    mock_requests.post.return_value = MockResponse(200)
+
+    revoke_cognito_refresh_token('refresh-token')
+
+    mock_requests.post.has_calls(call(ANY, {'token': 'refresh-token'}, ANY, {'Content-Type': 'application/x-www-form-urlencoded'}))
+
+
+
+
+def test_revoke_cognito_refresh_token_failing(mock_requests, mock_logger, caplog):
+    mock_requests.post.return_value = MockResponse(400)
+
+    revoke_cognito_refresh_token('refresh-token')
+
+    mock_requests.post.has_calls(call(ANY, {'token': 'refresh-token'}, ANY, {'Content-Type': 'application/x-www-form-urlencoded'}))
+    assert caplog.text.strip() == "WARNING  pcluster-manager:logger.py:27 {'message': 'Unable to revoke cognito refresh token'}"
