51c07176f04b8d02bacfdfa1e4d336473cb5e212
Barco <BarcoMasile@users.noreply.github.com>
2023-01-11 16:43:44 +0100

    Added validation for endpoints: push_log, login, price_estimate (#475)
    * added validation for login endpoint
    
    * validation on pushlog endpoint + refactor to remove custom validation
    
    * validation on priceestimate endpoint
    
    * improvement
    
    * removed tests that tested custom validation, no longer needed
    
    Co-authored-by: Marco Basile <mrbarco@amazon.com>

--- api/tests/test_push_log.py
+++ api/tests/test_push_log.py
@@ -6,6 +6,7 @@ import pytest
 def trim_log(caplog):
     return caplog.text.replace('\n', '')
 
+
 @pytest.fixture(scope='function')
 def disable_auth(monkeypatch):
     monkeypatch.setitem(environ, 'ENABLE_AUTH', 'false')
@@ -32,50 +33,3 @@ def test_push_log_controller_with_valid_json_with_extra(client, caplog, disable_
 
     assert response.status_code == 200
     assert trim_log(caplog) == expected_log
-
-
-def test_push_log_controller_with_invalid_message_key(client, disable_auth, mock_csrf_needed):
-    request_body = { 'logs': [{'message_wrong': 'sample-message', 'level': 'info'}]}
-
-    response = client.post('/logs', json=request_body)
-
-    assert response.status_code == 400
-    assert response.json == {
-            'code': 400,
-            'message': 'Request body missing one or more mandatory fields ["message", "level"]'
-        }
-
-
-def test_push_log_controller_with_invalid_level_key(client, disable_auth, mock_csrf_needed):
-    request_body = { 'logs': [{'message': 'sample-message', 'level_wrong': 'info'}]}
-
-    response = client.post('/logs', json=request_body)
-
-    assert response.status_code == 400
-    assert response.json == {
-            'code': 400,
-            'message': 'Request body missing one or more mandatory fields ["message", "level"]'
-        }
-
-
-def test_push_log_controller_with_invalid_level_value(client, disable_auth, mock_csrf_needed):
-    request_body = { 'logs': [{'message': 'sample-message', 'level': 'wrong-level'}]}
-
-    response = client.post('/logs', json=request_body)
-
-    assert response.status_code == 400
-    assert response.json == {
-            'code': 400,
-            'message': 'Level param must be a valid log level'
-        }
-
-def test_push_log_controller_with_invalid_extra_value(client, disable_auth, mock_csrf_needed):
-    request_body = { 'logs': [{'message': 'sample-message', 'level': 'info', 'extra': 'wrong-value'}]}
-
-    response = client.post('/logs', json=request_body)
-
-    assert response.status_code == 400
-    assert response.json == {
-            'code': 400,
-            'message': 'Extra param must be a valid json object'
-        }
