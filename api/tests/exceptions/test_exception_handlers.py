3fd2814f36db1334281386f065752b7a43ffb252
Barco <BarcoMasile@users.noreply.github.com>
2023-01-18 12:19:01 +0100

    Improved exception handler tests (#490)
    * refactored boto3 handler test to not rely on real endpoint
    
    * refactored value error handler test to not rely on real endpoint
    
    * refactored unauthenticated error handler test to not rely on real endpoint
    
    * refactored validation handler test to not rely on real endpoint
    
    * refactored global exception handler test to not rely on real endpoint
    
    * removed ugly slash from imports
    
    Co-authored-by: Marco Basile <mrbarco@amazon.com>

--- api/tests/exceptions/test_exception_handlers.py
+++ api/tests/exceptions/test_exception_handlers.py
@@ -1,28 +1,24 @@
 from marshmallow import ValidationError
 
 from api.exception.exceptions import RefreshTokenError, CSRFError
-from api.exception.handlers import csrf_error_handler
+from api.exception.handlers import csrf_error_handler, boto3_exception_handler, value_error_handler, unauthenticated_error_handler, validation_error_handler, global_exception_handler
 from api.security.csrf import CSRF_COOKIE_NAME
 
 
 def test_boto3_exception_handler(client, client_error_response, app, monkeypatch):
-    def delete_user_raising_clienterror():
-        raise client_error_response
+    with app.test_request_context('/'):
+        app.preprocess_request()
+        response, status_code = boto3_exception_handler(client_error_response)
 
-    monkeypatch.setitem(app.view_functions, 'delete_user_', delete_user_raising_clienterror)
-    response = client.delete('/manager/delete_user', json={'username': 'some-user'})
-
-    assert response.status_code == 400
+    assert status_code == 400
     assert response.json == {'code': 400, 'message': 'Something went wrong while invoking other AWS services'}
 
 def test_value_error_exception_handler(client, app, monkeypatch):
-    def push_log_raising():
-        raise ValueError('Validation error')
+    with app.test_request_context('/'):
+        app.preprocess_request()
+        response, status_code = value_error_handler(ValueError('Validation error'))
 
-    monkeypatch.setitem(app.view_functions, 'push_log', push_log_raising)
-    response = client.post('/logs')
-
-    assert response.status_code == 400
+    assert status_code == 400
     assert response.json == {'code': 400, 'message': 'Validation error'}
 
 def test_csrf_error_exception_handler(client, app, monkeypatch):
@@ -41,36 +37,28 @@ def test_csrf_error_exception_handler(client, app, monkeypatch):
 
 
 def test_unauthenticated_error_handler(client, app, monkeypatch):
+    with app.test_request_context('/'):
+        app.preprocess_request()
+        response, status_code = unauthenticated_error_handler(RefreshTokenError('refresh-token-error'))
 
-    def push_log_raising():
-        raise RefreshTokenError('refresh-token-error')
-
-    monkeypatch.setitem(app.view_functions, 'push_log', push_log_raising)
-    response = client.post('/logs')
-
-    assert response.status_code == 401
+    assert status_code == 401
     assert response.json == {'code': 401, 'message': 'Refresh token error: refresh-token-error'}
 
 def test_validation_error_handler(client, app, monkeypatch):
+    with app.test_request_context('/'):
+        app.preprocess_request()
+        response, status_code = validation_error_handler(ValidationError('Input validation failed for /manager/ec2_action', data={'field': ['validation-error']}))
 
-    def ec2_action_raising():
-        raise ValidationError('Input validation failed for /manager/ec2_action', data={'field': ['validation-error']})
-
-    monkeypatch.setitem(app.view_functions, 'ec2_action_', ec2_action_raising)
-    response = client.post('/manager/ec2_action')
-
-    assert response.status_code == 400
+    assert status_code == 400
     assert response.json == {
         'code': 400, 'message': 'Input validation failed for /manager/ec2_action',
         'validation_errors': {'field': ['validation-error']}
     }
 
 def test_global_exception_handler_with_app_logic(client, app, monkeypatch):
-    def get_app_config_raising_generic_exception():
-        raise Exception('generic exception')
+    with app.test_request_context('/'):
+        app.preprocess_request()
+        response, status_code = global_exception_handler(Exception('generic exception'))
 
-    monkeypatch.setitem(app.view_functions, 'get_app_config_', get_app_config_raising_generic_exception)
-    response = client.get('/manager/get_app_config')
-
-    assert response.status_code == 400
+    assert status_code == 400
     assert response.json == {'code': 400, 'message': 'An error occurred while trying to complete your request. Please try again later. If the problem persists, please contact support for further assistance.'}
