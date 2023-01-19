40691f562c102c41036bcfc0bbe5147beea7671e
Mattia Franchetto <mtfranco@amazon.it>
2023-01-19 11:33:14 +0100

    Use pbkdf2_hmac in fingerprint generation

--- api/tests/security/test_secret_generator.py
+++ api/tests/security/test_secret_generator.py
@@ -8,7 +8,7 @@ def test_cognito_fingerprint_generator():
         it should produce the same fingerprint everytime
     """
     client_id, client_secret, user_pool_id = 'client-id', 'client-secret', 'pool-id'
-    expected_fingerprint = '456fe4db680c3be20d9f7d000d5d2fdc5aff08668832760caac765aa2814bdd6'
+    expected_fingerprint = '88056a6f3236e82b05de9bbb01a979b2877563c0c971148bc20aaa9aad8b3b85'
 
     gen = CognitoFingerprintGenerator(client_id, client_secret, user_pool_id)
     fingerprint = gen.fingerprint()
