3e37f17b8b1232a16891d815dc5883c95015431d
Mattia Franchetto <mt.franchetto@gmail.com>
2023-01-24 11:13:29 +0100

    Copywriting fixes (#503)
    * strings.json editing
    
    * strings.json edits
    
    * Re-add labels to fix frontend build
    
    * Fix strings interpolation error
    
    * Fix unit and e2e tests
    
    Co-authored-by: Kari Stromsland <kstromsl@amazon.com>

--- frontend/src/old-pages/Clusters/__tests__/Clusters.test.tsx
+++ frontend/src/old-pages/Clusters/__tests__/Clusters.test.tsx
@@ -97,7 +97,7 @@ describe('given a component to show the clusters list', () => {
           ),
         )
 
-        await userEvent.click(output.getByText('Create Cluster'))
+        await userEvent.click(output.getByText('Create'))
         expect(mockNavigate).toHaveBeenCalledWith('/configure')
       })
     })
