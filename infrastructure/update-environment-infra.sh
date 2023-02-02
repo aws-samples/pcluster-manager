0811d17b7e7c84445b5ef571f6a3126641d87b62
Paolo Sacconier <paolo@sacconier.net>
2023-02-02 12:51:53 +0100

    Remove sensitive info (#544)
    * use gh secrets
    
    * use gh secrets also for prod
    
    * infra bucket dynamic lookup and documentation update
    
    * temporary trigger deploy from remove-sensitive-info branch to test actions
    
    * adding additional required policy
    
    * allow correct resource for description
    
    * fix cf stack update params handling
    
    * Revert "temporary trigger deploy from remove-sensitive-info branch to test actions"
    
    This reverts commit e0322d6c9d3e4d3bc3ad6888166ed4b6bb308e43.
    
    * typo fix

--- infrastructure/update-environment-infra.sh
+++ infrastructure/update-environment-infra.sh
@@ -21,18 +21,34 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 FILES=(SSMSessionProfile-cfn.yaml parallelcluster-ui-cognito.yaml parallelcluster-ui.yaml)
 ENVIRONMENT=$1
-. ${SCRIPT_DIR}/environments/${ENVIRONMENT}-variables.sh
+. "${SCRIPT_DIR}/environments/${ENVIRONMENT}-variables.sh"
+
+BUCKET=$(aws cloudformation describe-stack-resources \
+  --stack-name "${INFRA_BUCKET_STACK_NAME}" \
+  --logical-resource-id InfrastructureBucket \
+  --output json \
+  --query 'StackResources[0].PhysicalResourceId'\
+  | tr -d '"' )
 
 # The yaml files describing the infrastructure are uploaded to a private S3 bucket
 # and then used to update the CloudFormation stack, where the same bucket is passed as parameters.
 # This is done to make sure that we deploy all the changes to the infrastructure, and not only the changes
 # made to parallelcluster-ui.yaml (the parent stack)
-echo Uploading to: ${BUCKET}
+echo Uploading to: "${BUCKET}"
 for FILE in "${FILES[@]}"
 do
-  aws s3 cp ${SCRIPT_DIR}/${FILE} s3://${BUCKET}/${FILE}
+  aws s3 cp "${SCRIPT_DIR}/${FILE}" "s3://${BUCKET}/${FILE}"
 done
 
+BUCKET_URL="https://${BUCKET}.s3.${REGION}.amazonaws.com"
+CLI_INPUT_YAML=$(sed "s#BUCKET_URL_PLACEHOLDER#${BUCKET_URL}#g" "${SCRIPT_DIR}/environments/${ENVIRONMENT}-cfn-update-args.yaml")
+
 # Launches a new CFN update: the script hangs until the stack is updated
-AWS_PAGER="cat" aws cloudformation update-stack --cli-input-yaml file://${SCRIPT_DIR}/environments/${ENVIRONMENT}-cfn.yaml --stack-name ${STACK_NAME} --region ${REGION}
-aws cloudformation wait stack-update-complete --stack-name ${STACK_NAME} --region ${REGION}
+AWS_PAGER="cat" aws cloudformation update-stack \
+  --cli-input-yaml "${CLI_INPUT_YAML}" \
+  --stack-name "${STACK_NAME}" \
+  --region "${REGION}"
+
+aws cloudformation wait stack-update-complete \
+  --stack-name "${STACK_NAME}" \
+  --region "${REGION}"
